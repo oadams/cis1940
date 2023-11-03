2e44468f533bef141c23f980bc264a1b753a9dc3
Oliver Adams <oliver.adams@gmail.com>
2023-11-03 17:13:46 +1100

    Homework 1, exercises 2 and 3.

--- homework1.hs
+++ homework1.hs
@@ -26,9 +26,28 @@ toDigits n
 toDigitsRev :: Integer -> [Integer]
 toDigitsRev n = reverse $ toDigits n
 
+doubleEveryOther' :: [Integer] -> [Integer]
+doubleEveryOther' [] = []
+doubleEveryOther' (h:[]) = (h:[])
+doubleEveryOther' (h:h2:xs) = h:2*h2:(doubleEveryOther' xs)
+
+doubleEveryOther :: [Integer] -> [Integer]
+doubleEveryOther l = reverse $ doubleEveryOther' $ reverse l
+
+sumDigit :: Integer -> Integer
+sumDigit n
+    | n <= 0 = 0
+    | otherwise = sumDigit (n `div` 10) + (n `mod` 10)
+
+sumDigits :: [Integer] -> Integer
+sumDigits l = sum $ map sumDigit l
+
 main :: IO ()
 main = do
     print $ toDigits 1234
     print $ toDigitsRev 1234
     print $ toDigits 0
     print $ toDigits (-17)
+    print $ doubleEveryOther [8, 7, 6, 5]
+    print $ doubleEveryOther [1, 2, 3]
+    print $ sumDigits [16, 7, 12, 5]
