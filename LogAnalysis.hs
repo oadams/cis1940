d1b00380f62ea81ec961e197451b799b99695ff5
Oliver Adams <oliver.adams@gmail.com>
2023-11-04 13:45:02 +1100

    Refactored to be safer.

--- LogAnalysis.hs
+++ LogAnalysis.hs
@@ -2,30 +2,25 @@
 
 module LogAnalysis where
 
+import Text.Read (readMaybe)
+
 import Log
 
 parseMessage :: String -> LogMessage
-parseMessage msg
-  | msgType == "E" = LogMessage (Error first) (read (words msg !! 2) :: Int) (unwords $ drop 3 $ words msg) 
-  | msgType == "W" = LogMessage Warning first rest
-  | msgType == "I" = LogMessage Info first rest
-  | otherwise = Unknown msg
-  where
-    msgType = head $ words msg
-    first = read (words msg !! 1) :: Int
-    rest = unwords $ drop 2 $ words msg
-
+parseMessage msg = case words msg of
+    ("E":severity:timestamp:rest) ->
+        case (readMaybe severity, readMaybe timestamp) of
+            (Just sev, Just ts) -> LogMessage (Error sev) ts (unwords rest)
+            _ -> Unknown msg
+    ("W":timestamp:rest) ->
+        case readMaybe timestamp of
+            Just ts -> LogMessage Warning ts (unwords rest)
+            _ -> Unknown msg
+    ("I":timestamp:rest) ->
+        case readMaybe timestamp of
+            Just ts -> LogMessage Info ts (unwords rest)
+            _ -> Unknown msg
+    _ -> Unknown msg
 
 parse :: String -> [LogMessage]
 parse = map parseMessage . lines
-
---main = do
---  print $ parseMessage "a b c"
-
-
--- Questions:
--- 1. How do we handle the possibility of line not starting with I, W, or E without taining our code with maybes?
--- 2. How do we get the Error constructor to apply to the first to words while the others apply to only 1?
--- The first question really cuts at the heart of something I want to know about how Haskell interacts with real world data.
--- Should have read the assignment properly, that's what the Unknown constructor is for.
-
