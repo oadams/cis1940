23c0e1f524fe8445ad788dea2ef7658cb4e5dc37
Oliver Adams <oliver.adams@gmail.com>
2023-11-04 15:19:52 +1100

    Up to exercise 4

--- homework2/LogAnalysis.hs
+++ homework2/LogAnalysis.hs
@@ -24,3 +24,29 @@ parseMessage msg = case words msg of
 
 parse :: String -> [LogMessage]
 parse = map parseMessage . lines
+
+insert :: LogMessage -> MessageTree -> MessageTree
+insert (Unknown _) tree = tree
+insert msg Leaf = Node Leaf msg Leaf
+insert msg@(LogMessage _ ts _) tree@(Node left msg2@(LogMessage _ ts2 _) right)
+    | ts < ts2 = Node (insert msg left) msg2 right
+    | ts >= ts2 = Node left msg2 (insert msg right)
+    | otherwise = tree -- This made the compiler stop complaining about inexhaustive patterns
+insert _ tree@(Node _ (Unknown _) _) = tree -- This shouldn't happen
+
+build' :: [LogMessage] -> MessageTree -> MessageTree
+build' [] tree = tree
+build' (m:ms) tree = build' ms (insert m tree)
+
+build :: [LogMessage] -> MessageTree
+build ms = build' ms Leaf
+
+inOrder :: MessageTree -> [LogMessage]
+inOrder Leaf = []
+inOrder (Node left msg right) = (inOrder left) ++ [msg] ++ (inOrder right)
+
+main :: IO ()
+main = do
+    msgs <- take 10 . parse <$> readFile "error.log" 
+    print msgs
+    print $ inOrder $ build msgs
