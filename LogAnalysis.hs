4d7bb28bf87b93e46f729ae48b07d755ae95e513
Oliver Adams <oliver.adams@gmail.com>
2023-11-04 12:20:35 +1100

    Created partially copilot-driven solution to exercise 1 that I think could be refactored.

---
+++ LogAnalysis.hs
@@ -0,0 +1,27 @@
+{-# OPTIONS_GHC -Wall #-}
+
+module LogAnalysis where
+
+import Log
+
+parseMessage :: String -> LogMessage
+parseMessage msg
+  | msgType == "E" = LogMessage (Error (read (words msg !! 1) :: Int)) (read (words msg !! 2) :: Int) (unwords $ drop 3 $ words msg) 
+  | msgType == "W" = LogMessage Warning (read (words msg !! 1) :: Int) (unwords $ drop 2 $ words msg)
+  | msgType == "I" = LogMessage Info (read (words msg !! 1) :: Int) (unwords $ drop 2 $ words msg)
+  | otherwise = Unknown msg
+  where msgType = head $ words msg
+
+parse :: String -> [LogMessage]
+parse = map parseMessage . lines
+
+--main = do
+--  print $ parseMessage "a b c"
+
+
+-- Questions:
+-- 1. How do we handle the possibility of line not starting with I, W, or E without taining our code with maybes?
+-- 2. How do we get the Error constructor to apply to the first to words while the others apply to only 1?
+-- The first question really cuts at the heart of something I want to know about how Haskell interacts with real world data.
+-- Should have read the assignment properly, that's what the Unknown constructor is for.
+
