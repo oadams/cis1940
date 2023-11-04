b475f7e327fd99bfd48ce62b8604be93557f822b
Oliver Adams <oliver.adams@gmail.com>
2023-11-04 13:40:20 +1100

    tiny refactor.

--- LogAnalysis.hs
+++ LogAnalysis.hs
@@ -6,11 +6,15 @@ import Log
 
 parseMessage :: String -> LogMessage
 parseMessage msg
-  | msgType == "E" = LogMessage (Error (read (words msg !! 1) :: Int)) (read (words msg !! 2) :: Int) (unwords $ drop 3 $ words msg) 
-  | msgType == "W" = LogMessage Warning (read (words msg !! 1) :: Int) (unwords $ drop 2 $ words msg)
-  | msgType == "I" = LogMessage Info (read (words msg !! 1) :: Int) (unwords $ drop 2 $ words msg)
+  | msgType == "E" = LogMessage (Error first) (read (words msg !! 2) :: Int) (unwords $ drop 3 $ words msg) 
+  | msgType == "W" = LogMessage Warning first rest
+  | msgType == "I" = LogMessage Info first rest
   | otherwise = Unknown msg
-  where msgType = head $ words msg
+  where
+    msgType = head $ words msg
+    first = read (words msg !! 1) :: Int
+    rest = unwords $ drop 2 $ words msg
+
 
 parse :: String -> [LogMessage]
 parse = map parseMessage . lines
